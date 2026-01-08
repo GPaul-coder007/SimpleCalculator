301fe0bd35caea02013f205657196ee251ee5a2d
G Paul Sharon <jacksparrow532005@gmail.com>
2026-01-08 13:32:16 +0530

    Update fetch URLs to use HTTPS

--- script.js
+++ script.js
@@ -3,25 +3,22 @@ const display = document.getElementById("display");
 function appendValue(value) {
     display.value += value;
 }
-
 function clearDisplay() {
     display.value = "";
 }
-
 function backspace() {
     display.value = display.value.slice(0, -1);
 }
-
 function calculate() {
     const expression = display.value;
+   fetch("https://127.0.0.1:5000/calculate", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ expression })
+})
 
-    fetch("http://127.0.0.1:5000/calculate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ expression: expression })
-    })
     .then(res => res.json())
    .then(data => {
     display.value = data.result;
@@ -32,7 +29,7 @@ function calculate() {
     });
 }
 function loadHistory() {
-    fetch("http://127.0.0.1:5000/history")
+    fetch("https://127.0.0.1:5000/history")
         .then(res => res.json())
         .then(data => {
             const historyList = document.getElementById("historyList");
